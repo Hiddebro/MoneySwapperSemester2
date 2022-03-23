2da8f12094fc1f4a7bd240e7ee5884827a6e3187
Hiddebro <hiddebr9@gmail.com>
2022-03-23 14:00:48 +0100

    adding dtos start

--- MoneyswapperDHL/UserContext.cs
+++ MoneyswapperDHL/UserContext.cs
@@ -3,11 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace MoneyswapperDAL
 {
-    internal class UserContext
+    public class UserContext
     {
+        string connectionString = "Data Source=mssqlstud.fhict.local;Initial Catalog=dbi439802_runescape;User ID=dbi439802_runescape;Password=Hidde";
 
-    }
-}
+        public UserDTO getUser(string username)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+               
+                con.Open();
+                SqlCommand comm = new SqlCommand("SELECT * FROM UserData WHERE Username = @username", con);
+
+                comm.Parameters.AddWithValue("@username", username.Trim());
+
+                SqlDataAdapter sda = new SqlDataAdapter(comm);
+                comm.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                var row = dt.Rows[0];
+               
+                return new UserDTO
+                {
+                    UserID = row.Field<int>("UserID"),
+                    OSRS = row.Field<int>("OSRS"),
+                    RS3 = row.Field<int>("RS3"),
+                    Email = row.Field<string>("Email"),
+                    Username = row.Field<string>("Username"),
+                    Password = row.Field<string>("Password")
+                };
+               
+             }
+               
+            
+        }
+
+            public bool Login(string username, string password)
+            {
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    
+                        con.Open();
+                        SqlCommand comm = new SqlCommand("SELECT * FROM UserData WHERE Username = @username AND Password = @password", con);
+                        SqlDataAdapter sda = new SqlDataAdapter(comm);
+
+                        comm.Parameters.AddWithValue("@username", username.Trim());
+                        comm.Parameters.AddWithValue("@password", password.Trim());
+
+                        comm.ExecuteNonQuery();
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+
+
+                        con.Close();
+
+                        return dt.Rows.Count != 0;
+                    }
+
+                }
+
+            }
+        }
+    
--- moneyswapper/User_container.cs
+++ moneyswapper/User_container.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MoneyswapperDAL;
 
 namespace moneyswapper
 {
     public class User_container
     {
-
+        UserContext userContext = new UserContext();
+           
+        public void GetUser()
+        {
+            userContext.getUser();  
+        }
     }
 }
